5b252f59484f7c1fd5f36f60764fdd18f15db618
MrRamis <99258165+MrRamis@users.noreply.github.com>
2024-01-28 20:23:28 +0300

    addTypCabinet

---
+++ WPFDecstop/vm/TypCabinet/AddTypCabinet.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WPFDecstop.Models;
+
+namespace WPFDecstop.vm.TypCabinet;
+
+public partial class AddTypCabinet : Page
+{
+    public AddTypCabinet()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick_Add_an_account_type(object sender, RoutedEventArgs e)
+    {
+        using (TimetableContext db = new TimetableContext())
+        {
+            CabinetType cabinetType = new CabinetType()
+            {
+                CabinetName = Cabinet_Type_Name_TextBox.Text,
+                Discription = Description_TextBox.Text
+            };
+            db.CabinetTypes.Add(cabinetType);
+            db.SaveChanges();
+        }
+        NavigationService.Navigate(null);
+    }
+}
